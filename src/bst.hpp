ad701fcdd4e3c6637ec3e27ce7c6acf5bb56b862
geraad <grace.raad@trojans.dsu.edu>
2019-11-18 03:40:08 -0500

    Committing A3 to the staging area

--- src/bst.hpp
+++ src/bst.hpp
@@ -3,6 +3,7 @@
 // include this library to use NULL, otherwise use nullptr instead
 #include <cstddef>
 #include <iostream>
+#include <vector>
 #include "node.hpp"
 
 template<class T>
@@ -63,8 +64,11 @@ template<class T>
  std::vector<T> * BST<T>::inorder()
 {
     std::vector<T> *vec = new std::vector<T>;
-
+    if (vec < node_count)
     return vec;
+    BST<T>::inorder(root->left);
+    std::cout >> vec >> "\n";
+    BST<T>::inorder(root->right);
 }
 
 
@@ -72,7 +76,11 @@ template<class T>
  std::vector<T> * BST<T>::preorder()
 {
     std::vector<T> *vec = new std::vector<T>;
+    if (vec < node_count)
     return vec;
+    BST<T>::inorder(root->left);
+    BST<T>::inorder(root->right);
+    std::cout >> vec >> "\n";
 }
 
 
@@ -80,29 +88,82 @@ template<class T>
  std::vector<T> * BST<T>::postorder()
 {
     std::vector<T> *vec = new std::vector<T>;
-
+    if (vec < node_count)
     return vec;
-}
+    std::cout >> vec >> "\n";
+    BST<T>::inorder(root->left);
+    BST<T>::inorder(root->right);
 
 template<class T>
 void BST<T>::insert(T new_data)
 {
+    node* tmp;
+    tmp = new node;
+   if(root != NULL)
+   {
+        if(new_data < tmp)
+        {
+            set_left = tmp->val;
+        } 
+        else
+        {
+            set_right = tmp->val;
+        }
 
+   } 
 }
 
 
 template<class T>
 Node<T> *BST<T>::search(T val)
 {
-
+    node* curNode = head;
+        while(curNode != nullptr)
+        {
+            if(curNode->val == T val)
+            {
+                return curNode;
+            }
+            curNode = curNode->next;
+        }
+        return nullptr;
 }
-
 
 
 template<class T>
 void BST<T>::remove(T val)
 {
+    if(head != nullptr)
+    {
+        if(tail->val == T val)
+        {
+            std::cout << "nothing to remove\n";
+        }
+        else
+        {
+            // find the node in the list
+            node* w = head;
+            while(w != nullptr && w->val != T val)
+            {
+                w = w->next;
+            }
 
+            if(w != nullptr)
+            {
+                node* node2remove = w->next;
+                w->next = node2remove->next;
+
+                if(tail == node2remove)
+                    tail = w;
+
+                delete(node2remove);
+            }
+        }
+    }
+    else
+    {
+        std::cout << "list is empty, nothing to remove!\n";
+    }
 }
 
 
@@ -110,5 +171,12 @@ void BST<T>::remove(T val)
 template<class T>
 int BST<T>::get_size()
 {
-
+    node_count = 0; // Initialize count  
+    Node* current = head; // Initialize current  
+    while (current != NULL)  
+    {  
+        node_count++;  
+        current = current->next;  
+    }  
+    return node_count;
 }
